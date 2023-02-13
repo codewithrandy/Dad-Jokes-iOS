2ad9822878d62690052b40258cea5147439c974f
Randy McKown <72272050+nerdvoodoo@users.noreply.github.com>
2023-02-13 13:21:56 -0600

    Initial Commit

---
+++ DadJokes/DadJokesApp.swift
@@ -0,0 +1,17 @@
+//
+//  DadJokesApp.swift
+//  DadJokes
+//
+//  Created by Randy McKown on 2/13/23.
+//
+
+import SwiftUI
+
+@main
+struct DadJokesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
